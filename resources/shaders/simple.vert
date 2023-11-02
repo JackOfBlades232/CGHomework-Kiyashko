69472d298f7b0425a5c1ce0df1e7a20e3c74aa95
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-11-02 23:41:46 +0200

    Something works, and something is fucked up bad))

--- resources/shaders/simple.vert
+++ resources/shaders/simple.vert
@@ -2,6 +2,7 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_GOOGLE_include_directive : require
 
+#include "common.h"
 #include "unpack_attributes.h"
 
 layout(location = 0) in vec4 vPosNorm;
@@ -9,16 +10,24 @@ layout(location = 1) in vec4 vTexCoordAndTang;
 
 layout(push_constant) uniform params_t {
     mat4 mProjView;
-    mat4 mModel;
+    uint mModelIndex;
 } params;
 
-
-layout (location = 0 ) out VS_OUT {
+layout(location = 0) out VS_OUT {
     vec3 wPos;
     vec3 wNorm;
     vec3 wTangent;
     vec2 texCoord;
 } vOut;
+
+layout(binding = 0, set = 0) uniform InstanceData {
+    // @TODO: fix this
+    mat4 instanceMatrices[27];
+};
+
+layout(binding = 1, set = 0) uniform AppData {
+    UniformParams Params;
+};
 
 out gl_PerVertex { vec4 gl_Position; };
 void main(void)
@@ -26,9 +35,15 @@ void main(void)
     const vec4 wNorm = vec4(DecodeNormal(floatBitsToInt(vPosNorm.w)),         0.0f);
     const vec4 wTang = vec4(DecodeNormal(floatBitsToInt(vTexCoordAndTang.z)), 0.0f);
 
+    /*
     vOut.wPos     = (params.mModel * vec4(vPosNorm.xyz, 1.0f)).xyz;
     vOut.wNorm    = normalize(mat3(transpose(inverse(params.mModel))) * wNorm.xyz);
     vOut.wTangent = normalize(mat3(transpose(inverse(params.mModel))) * wTang.xyz);
+    vOut.texCoord = vTexCoordAndTang.xy;
+    */
+    vOut.wPos     = (instanceMatrices[params.mModelIndex] * vec4(vPosNorm.xyz, 1.0f)).xyz;
+    vOut.wNorm    = normalize(mat3(transpose(inverse(instanceMatrices[params.mModelIndex]))) * wNorm.xyz);
+    vOut.wTangent = normalize(mat3(transpose(inverse(instanceMatrices[params.mModelIndex]))) * wTang.xyz);
     vOut.texCoord = vTexCoordAndTang.xy;
 
     gl_Position   = params.mProjView * vec4(vOut.wPos, 1.0);
--- src/render/scene_mgr.h
+++ src/render/scene_mgr.h
@@ -11,92 +11,92 @@
 #include "../loader_utils/hydraxml.h"
 #include "../resources/shaders/common.h"
 
-struct InstanceInfo
-{
-  uint32_t inst_id = 0u;
-  uint32_t mesh_id = 0u;
-  VkDeviceSize instBufOffset = 0u;
-  bool renderMark = false;
+struct InstanceInfo {
+    uint32_t inst_id = 0u;
+    uint32_t mesh_id = 0u;
+    VkDeviceSize instBufOffset = 0u;
+    bool renderMark = false;
 };
 
-struct SceneManager
-{
-  SceneManager(VkDevice a_device, VkPhysicalDevice a_physDevice, uint32_t a_transferQId, uint32_t a_graphicsQId,
-    bool debug = false);
-  ~SceneManager() { DestroyScene(); }
+struct SceneManager {
+    SceneManager(VkDevice a_device, VkPhysicalDevice a_physDevice, uint32_t a_transferQId, uint32_t a_graphicsQId,
+            bool debug = false);
+    ~SceneManager() { DestroyScene(); }
 
-  bool LoadSceneXML(const std::string &scenePath, bool transpose = true);
-  void LoadSingleTriangle();
+    bool LoadSceneXML(const std::string &scenePath, bool transpose = true);
+    void LoadSingleTriangle();
 
-  uint32_t AddMeshFromFile(const std::string& meshPath);
-  uint32_t AddMeshFromData(cmesh::SimpleMesh &meshData);
+    uint32_t AddMeshFromFile(const std::string& meshPath);
+    uint32_t AddMeshFromData(cmesh::SimpleMesh &meshData);
 
-  uint32_t InstanceMesh(uint32_t meshId, const LiteMath::float4x4 &matrix, bool markForRender = true);
+    uint32_t InstanceMesh(uint32_t meshId, const LiteMath::float4x4 &matrix, bool markForRender = true);
 
-  void MarkInstance(uint32_t instId);
-  void UnmarkInstance(uint32_t instId);
+    void MarkInstance(uint32_t instId);
+    void UnmarkInstance(uint32_t instId);
 
-  void DrawMarkedInstances();
+    void DrawMarkedInstances();
 
-  void DestroyScene();
+    void DestroyScene();
 
-  etna::VertexByteStreamFormatDescription GetVertexStreamDescription();
+    etna::VertexByteStreamFormatDescription GetVertexStreamDescription();
 
-  VkBuffer GetVertexBuffer() const { return m_geoVertBuf; }
-  VkBuffer GetIndexBuffer()  const { return m_geoIdxBuf; }
-  VkBuffer GetMeshInfoBuffer()  const { return m_meshInfoBuf; }
-  VkBuffer GetInstanceBuffer()  const { return m_instanceMatricesBuf; }
-  std::shared_ptr<vk_utils::ICopyEngine> GetCopyHelper() { return  m_pCopyHelper; }
+    VkBuffer GetVertexBuffer() const { return m_geoVertBuf; }
+    VkBuffer GetIndexBuffer()  const { return m_geoIdxBuf; }
+    VkBuffer GetMeshInfoBuffer()  const { return m_meshInfoBuf; }
+    VkBuffer GetInstanceBuffer()  const { return m_instanceMatricesBuf; }
+    std::shared_ptr<vk_utils::ICopyEngine> GetCopyHelper() { return  m_pCopyHelper; }
 
-  uint32_t MeshesNum() const {return (uint32_t)m_meshInfos.size();}
-  uint32_t InstancesNum() const {return (uint32_t)m_instanceInfos.size();}
+    uint32_t MeshesNum() const {return (uint32_t)m_meshInfos.size();}
+    uint32_t InstancesNum() const {return (uint32_t)m_instanceInfos.size();}
 
-  hydra_xml::Camera GetCamera(uint32_t camId) const;
-  MeshInfo GetMeshInfo(uint32_t meshId) const {assert(meshId < m_meshInfos.size()); return m_meshInfos[meshId];}
-  LiteMath::Box4f GetMeshBbox(uint32_t meshId) const {assert(meshId < m_meshBboxes.size()); return m_meshBboxes[meshId];}
-  InstanceInfo GetInstanceInfo(uint32_t instId) const {assert(instId < m_instanceInfos.size()); return m_instanceInfos[instId];}
-  LiteMath::Box4f GetInstanceBbox(uint32_t instId) const {assert(instId < m_instanceBboxes.size()); return m_instanceBboxes[instId];}
-  LiteMath::float4x4 GetInstanceMatrix(uint32_t instId) const {assert(instId < m_instanceMatrices.size()); return m_instanceMatrices[instId];}
-  LiteMath::Box4f GetSceneBbox() const {return sceneBbox;}
+    hydra_xml::Camera GetCamera(uint32_t camId) const;
+    MeshInfo GetMeshInfo(uint32_t meshId) const {assert(meshId < m_meshInfos.size()); return m_meshInfos[meshId];}
+    LiteMath::Box4f GetMeshBbox(uint32_t meshId) const {assert(meshId < m_meshBboxes.size()); return m_meshBboxes[meshId];}
+    InstanceInfo GetInstanceInfo(uint32_t instId) const {assert(instId < m_instanceInfos.size()); return m_instanceInfos[instId];}
+    LiteMath::Box4f GetInstanceBbox(uint32_t instId) const {assert(instId < m_instanceBboxes.size()); return m_instanceBboxes[instId];}
+    LiteMath::float4x4 GetInstanceMatrix(uint32_t instId) const {assert(instId < m_instanceMatrices.size()); return m_instanceMatrices[instId];}
+    LiteMath::Box4f GetSceneBbox() const {return sceneBbox;}
+
+    std::vector<LiteMath::float4x4> *GetInstanceMatrices() { return &m_instanceMatrices; }
+    std::vector<LiteMath::Box4f> *GetInstanceBboxes() { return &m_instanceBboxes; }
 
 private:
-  void LoadGeoDataOnGPU();
+    void LoadGeoDataOnGPU();
 
-  std::vector<MeshInfo> m_meshInfos = {};
-  std::vector<LiteMath::Box4f> m_meshBboxes = {};
-  std::shared_ptr<IMeshData> m_pMeshData = nullptr;
+    std::vector<MeshInfo> m_meshInfos = {};
+    std::vector<LiteMath::Box4f> m_meshBboxes = {};
+    std::shared_ptr<IMeshData> m_pMeshData = nullptr;
 
-  std::vector<InstanceInfo> m_instanceInfos = {};
-  std::vector<LiteMath::Box4f> m_instanceBboxes = {};
-  std::vector<LiteMath::float4x4> m_instanceMatrices = {};
+    std::vector<InstanceInfo> m_instanceInfos = {};
+    std::vector<LiteMath::Box4f> m_instanceBboxes = {};
+    std::vector<LiteMath::float4x4> m_instanceMatrices = {};
 
-  std::vector<hydra_xml::Camera> m_sceneCameras = {};
-  LiteMath::Box4f sceneBbox;
+    std::vector<hydra_xml::Camera> m_sceneCameras = {};
+    LiteMath::Box4f sceneBbox;
 
-  uint32_t m_totalVertices = 0u;
-  uint32_t m_totalIndices  = 0u;
+    uint32_t m_totalVertices = 0u;
+    uint32_t m_totalIndices  = 0u;
 
-  VkBuffer m_geoVertBuf = VK_NULL_HANDLE;
-  VkBuffer m_geoIdxBuf  = VK_NULL_HANDLE;
-  VkBuffer m_meshInfoBuf  = VK_NULL_HANDLE;
-  VkBuffer m_instanceMatricesBuf = VK_NULL_HANDLE;
-  VkDeviceMemory m_geoMemAlloc = VK_NULL_HANDLE;
+    VkBuffer m_geoVertBuf = VK_NULL_HANDLE;
+    VkBuffer m_geoIdxBuf  = VK_NULL_HANDLE;
+    VkBuffer m_meshInfoBuf  = VK_NULL_HANDLE;
+    VkBuffer m_instanceMatricesBuf = VK_NULL_HANDLE;
+    VkDeviceMemory m_geoMemAlloc = VK_NULL_HANDLE;
 
-  VkDevice m_device = VK_NULL_HANDLE;
-  VkPhysicalDevice m_physDevice = VK_NULL_HANDLE;
-  uint32_t m_transferQId = UINT32_MAX;
-  VkQueue m_transferQ = VK_NULL_HANDLE;
+    VkDevice m_device = VK_NULL_HANDLE;
+    VkPhysicalDevice m_physDevice = VK_NULL_HANDLE;
+    uint32_t m_transferQId = UINT32_MAX;
+    VkQueue m_transferQ = VK_NULL_HANDLE;
 
-  uint32_t m_graphicsQId = UINT32_MAX;
-  VkQueue m_graphicsQ = VK_NULL_HANDLE;
-  std::shared_ptr<vk_utils::ICopyEngine> m_pCopyHelper;
+    uint32_t m_graphicsQId = UINT32_MAX;
+    VkQueue m_graphicsQ = VK_NULL_HANDLE;
+    std::shared_ptr<vk_utils::ICopyEngine> m_pCopyHelper;
 
-  bool m_debug = false;
-  // for debugging
-  struct Vertex
-  {
-    float pos[3];
-  };
+    bool m_debug = false;
+    // for debugging
+    struct Vertex {
+        float pos[3];
+    };
 };
 
 #endif//CHIMERA_SCENE_MGR_H
